1d3eff4866bbaaaa5c877258d13c029fc7f9f76f
imad bg <imadbg01@users.noreply.github.com>
2022-07-06 17:04:55 +0100

    fix - config vite for multi pages '

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        destination: resolve(__dirname, "pages/destination.html"),
+        crew: resolve(__dirname, "pages/crew.html"),
+        technology: resolve(__dirname, "pages/technology.html"),
+      },
+    },
+  },
+});
