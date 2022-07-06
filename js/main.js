bcf556bf948660edf588534297b956beb9ef84cf
imad bg <imadbg01@users.noreply.github.com>
2022-07-06 22:02:12 +0100

    add - 'functionality to destination page'

--- js/main.js
+++ js/main.js
@@ -15,3 +15,72 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", "false");
   }
 });
+
+// fetch json data
+
+const getData = async () => {
+  let data = {};
+
+  fetch("../data/data.json")
+    .then((res) => {
+      const contentType = res.headers.get("content-type");
+      if (!contentType || !contentType.includes("application/json")) {
+        throw new TypeError("Oops, This page has no JSON data!");
+      }
+      return res.json();
+    })
+    .then((json) => {
+      data = json;
+      const tabList = document.querySelector("[role='tablist']");
+      const tabs = document.querySelectorAll("[role='tab']");
+
+      const pageLabel = tabList
+        ? tabList.getAttribute("aria-label")
+        : console.log("not tabList found");
+      let page = data[pageLabel];
+
+      let pageFieldIds = [];
+
+      if (pageLabel === "destinations") {
+        pageFieldIds = ["name", "image", "description", "distance", "travel"];
+      } else {
+        pageFieldIds = ["name", "image", "role", "bio"];
+      }
+
+      tabs.forEach((tab) => {
+        tab.addEventListener("click", changeTapContent);
+      });
+
+      function changeTapContent(e) {
+        const firstLetterUpperCase = (str) =>
+          str.charAt(0).toUpperCase() + str.slice(1);
+        const targetElement = e.target;
+        const targetName = targetElement.getAttribute("aria-controls");
+        const prevActive = targetElement.parentNode.querySelector(
+          '[aria-selected="true"]'
+        );
+
+        const [objectData] = page.filter(
+          (el) => el.name === firstLetterUpperCase(targetName)
+        );
+
+        prevActive.setAttribute("aria-selected", false);
+        targetElement.setAttribute("aria-selected", true);
+        changeContent(objectData);
+      }
+
+      function changeContent(data) {
+        pageFieldIds.forEach((fieldId) => {
+          const elementId = document.querySelector(`#${fieldId}`);
+          if (fieldId === "image") {
+            elementId.src = data[fieldId];
+            elementId.setAttribute("alt", `${data.name}-${page}`);
+          } else {
+            elementId.textContent = data[fieldId];
+          }
+        });
+      }
+    });
+};
+
+getData();
